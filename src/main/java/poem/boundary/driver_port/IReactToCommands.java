7b2a7e2837099cf8a60b56776f97e7fcaf48c272
Bertil Muth <bertilmuth@yahoo.de>
2019-07-04 21:58:22 +0200

    Update Javadoc

--- src/main/java/poem/boundary/driver_port/IReactToCommands.java
+++ src/main/java/poem/boundary/driver_port/IReactToCommands.java
@@ -5,11 +5,11 @@ import java.util.concurrent.CompletableFuture;
 public interface IReactToCommands{
 	/**
 	 * Dispatches the specified command asynchronously to a command handler,
-	 * and returns the events published as a result.
+	 * and returns the event published as a result.
 	 * 
 	 * @param commandObject the command
 	 * 
-	 * @return a future to access
+	 * @return a future to access the resulting event
 	 */
 	public CompletableFuture<Void> reactTo(Object commandObject);
 }
